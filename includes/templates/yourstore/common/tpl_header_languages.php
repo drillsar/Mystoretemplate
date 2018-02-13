53513ba9f3abaec9fe39a3bb848d981f6982b68e
drillsar <travelagloo@aol.com>
2018-02-13 00:09:12 -0500

    Php 7.2

--- includes/templates/yourstore/common/tpl_header_languages.php
+++ includes/templates/yourstore/common/tpl_header_languages.php
@@ -11,7 +11,7 @@
  ?> 
  <?php 
  //for ($i=0; $i<4; $i++) {
-  while (list($key, $value) = each($lng->catalog_languages)) {
+  foreach ($lng->catalog_languages) {
     $content1 = '<li class="languages__item '.(($_SESSION['languages_code']==$key)? 'active' : '').'"><a href="' . zen_href_link($_GET['main_page'], zen_get_all_get_params(array('language', 'currency')) . 'language=' . $key, $request_type) . '">' . zen_image(DIR_WS_LANGUAGES .  $value['directory'] . '/images/' . $value['image']). '&nbsp;' . $value['name'] . '</a></li>';
  echo $content1;
  }
