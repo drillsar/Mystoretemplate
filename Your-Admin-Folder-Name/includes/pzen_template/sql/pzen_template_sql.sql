889a91c438387f4bf87374eaeb8a29ef44c7e75e
drillsar <travelagloo@aol.com>
2018-02-14 03:44:31 -0500

    Update Sql; Issue #14
    Todo remove

--- Your-Admin-Folder-Name/includes/pzen_template/sql/pzen_template_sql.sql
+++ Your-Admin-Folder-Name/includes/pzen_template/sql/pzen_template_sql.sql
@@ -424,20 +424,21 @@ INSERT INTO configuration (configuration_title, configuration_key, configuration
 
 
 
-INSERT INTO configuration_group (configuration_group_title,configuration_group_description,sort_order,visible) VALUES ('Disqus Comments', 'Disqus Comments Configuration', '1', '1');
-UPDATE configuration_group SET sort_order = last_insert_id() WHERE configuration_group_id = last_insert_id();
+#INSERT INTO configuration_group (configuration_group_title,configuration_group_description,sort_order,visible) VALUES ('Disqus Comments', 'Disqus Comments Configuration', '1', '1');
+#UPDATE configuration_group SET sort_order = last_insert_id() WHERE configuration_group_id = last_insert_id();
 
-SET @t4=0;
-SELECT (@t4:=configuration_group_id) as t4 
-FROM configuration_group
-WHERE configuration_group_title= 'Disqus Comments';
+##TODO REMOVE
+#SET @t4=0;
+#SELECT (@t4:=configuration_group_id) as t4 
+#FROM configuration_group
+#WHERE configuration_group_title= 'Disqus Comments';
 
-INSERT INTO configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, last_modified, date_added, use_function, set_function) VALUES 
-('Enable Disqus', 'DISQUS_STATUS', 'true', 'Turn On/Off Disqus Comments System', @t4, 1, NOW(), NOW(), NULL, 'zen_cfg_select_option(array(\'true\', \'false\'),'),
-('Site Shortname', 'DISQUS_SHORTNAME', 'perfectus-themes', 'Your Disqus Site Shortname. Sign Up <a href="http://disqus.com/" target="_blank">here</a> if you do not have it yet.', @t4, 2, NOW(), NOW(), NULL, NULL),
-('Element ID', 'DISQUS_ELEMENT_ID', 'disqus_thread', 'Disqus HTML Element ID, this the place where disqus appear. Default is "disqus_thread"', @t4, 3, NOW(), NOW(), NULL, NULL);
+#INSERT INTO configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, last_modified, date_added, use_function, set_function) VALUES 
+#('Enable Disqus', 'DISQUS_STATUS', 'true', 'Turn On/Off Disqus Comments System', @t4, 1, NOW(), NOW(), NULL, 'zen_cfg_select_option(array(\'true\', \'false\'),'),
+#('Site Shortname', 'DISQUS_SHORTNAME', 'perfectus-themes', 'Your Disqus Site Shortname. Sign Up <a href="http://disqus.com/" target="_blank">here</a> if you do not have it yet.', @t4, 2, NOW(), NOW(), NULL, NULL),
+#('Element ID', 'DISQUS_ELEMENT_ID', 'disqus_thread', 'Disqus HTML Element ID, this the place where disqus appear. Default is "disqus_thread"', @t4, 3, NOW(), NOW(), NULL, NULL);
 
-INSERT INTO admin_pages (page_key, language_key, main_page, page_params, menu_key, display_on_menu, sort_order) VALUES ('configDisqus', 'BOX_CONFIGURATION_DISQUS', 'FILENAME_CONFIGURATION', CONCAT('gID=',@t4), 'configuration', 'Y', 100);
+#INSERT INTO admin_pages (page_key, language_key, main_page, page_params, menu_key, display_on_menu, sort_order) VALUES ('configDisqus', 'BOX_CONFIGURATION_DISQUS', 'FILENAME_CONFIGURATION', CONCAT('gID=',@t4), 'configuration', 'Y', 100);
 
 
 DELETE FROM configuration_group WHERE configuration_group_title LIKE 'Manufacturers All Config' LIMIT 2;
